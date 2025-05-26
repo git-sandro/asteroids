53d3a3119aa603bf3cc1f297e384643efeb9a632
git-sandro <sandro-zimmermann@outlook.com>
2025-05-26 17:56:21 +0200

    update main.py to origin

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
         screen.fill("black")
         pygame.display.flip()
 
